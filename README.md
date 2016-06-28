1a260aded431de983b9ab4a3dd775284c7034484
Jason Hills <jasonahills@gmail.com>
2016-06-28 14:41:52 -0600

    adds random-file.sh

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Some scripts to make it easier to read random old journal entries.
 - Add something like the following to your `.bashrc` or `.bash_profile`:
 
 ```bash
-alias random-read="bash ~/path/to/random-reader/read.sh"
+alias random-file="bash ~/path/to/random-reader/random-file.sh"
 alias random-write="bash ~/path/to/random-reader/write.sh"
-alias til-read="random-read ~/path/to/til"
+alias til-read="random-file '*.md' ~/path/to/til | xargs cat"
 alias til="random-write ~/path/to/til"
 ```
---
+++ random-file.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check args
+if [ -z "$1" ]; then
+    echo "Must include file pattern to match" 1>&2;
+    exit 1;
+else
+    findPattern=$1;
+fi
+
+if [ -z "$2" ]; then
+    echo "Must include directory to read from" 1>&2;
+    exit 1;
+else
+    directory=$2;
+fi
+
+
+# Find files in directory
+array=(`find $directory -name $findPattern`);
+numDocs=${#array[@]};
+
+if [ $numDocs -eq 0 ]; then
+    echo "Directory is empty" 1>&2;
+    exit 1;
+fi
+
+
+# Randomly select a file.
+index=$(($RANDOM % numDocs));  #$RANDOM is only 16-bit number; might want a better random.
+echo ${array[index]}
